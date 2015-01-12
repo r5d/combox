286421c30599dee1f1aaee500f3bfed0bada6c5f
Siddharth Ravikumar <sravik@bgsu.edu>
2015-01-12 16:08:26 -0500

    added  setup.py

---
+++ setup.py
@@ -0,0 +1,38 @@
+#    Copyright (C) 2014 Combox author(s). See AUTHORS.
+#
+#    This file is part of Combox.
+#
+#   Combox is free software: you can redistribute it and/or modify it
+#   under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   Combox is distributed in the hope that it will be useful, but
+#   WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#   General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with Combox (see COPYING).  If not, see
+#   <http://www.gnu.org/licenses/>.
+
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+config = {
+    'description': 'Splits encrypted files between online file storage providers',
+    'author': 'Siddharth Ravikumar',
+    'url': '',
+    'download_url': 'Where to download it.',
+    'author_email': 'sravik@bgsu.edu',
+    'version': '0.0.0',
+    'install_requires': ['nose', 'watchdog', 'PyYAML'],
+    'packages': ['combox'],
+    'scripts': [],
+    'name': 'combox'
+    }
+
+setup(**config)
