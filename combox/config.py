6b56bb9eeb303cd7121d8c8fc8b7e791b0cffa63
Siddharth Ravikumar <sravik@bgsu.edu>
2015-01-21 07:08:18 -0500

    now the config file also makes note of the 'silo' directory.
    silo directory is the location where the silo (silo.db) containing
    information about files in the combox directory is stored.
    
    	modified:   combox/config.py
    	modified:   tests/config_test.py
    	modified:   tests/test-config/config.yaml

--- combox/config.py
+++ combox/config.py
@@ -76,6 +76,13 @@ def config_cb(config_dir = path.join(os.getenv('HOME'),'.combox/'),
     config_info = {}
 
     config_info['combox_dir'] = path.abspath(input_func('path to combox directory'))
+
+    silo_help_txt = 'silo directory (default: %s)' % (config_dir)
+    config_info['silo_dir'] = path.abspath(input_func(silo_help_txt))
+
+    if not config_info['silo_dir']:
+        config_info['silo_dir'] = config_dir
+
     config_info['topsecret'] = pass_func()
 
     no_nodes = int(input_func('number of nodes'))
