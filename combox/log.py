0cfeecae844dc1f16f5d5dac8a3cd91ae9d34675
Siddharth Ravikumar <sravik@bgsu.edu>
2015-10-15 22:38:11 -0400

    added combox/log.py: module for combox to log stuff.

---
+++ combox/log.py
@@ -0,0 +1,28 @@
+#    Copyright (C) 2015 Combox author(s). See AUTHORS.
+#
+#    This file is part of Combox.
+#
+#   Combox is free software: you can redistribute it and/or modify it
+#   under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   Combox is distributed in the hope that it will be useful, but
+#   WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#   General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with Combox (see COPYING).  If not, see
+#   <http://www.gnu.org/licenses/>.
+
+import logging
+
+
+def log_i(msg):
+    """Methdod for logging information.
+    """
+    logging.basicConfig(level=logging.INFO,
+                        format='%(asctime)s - %(message)s',
+                        datefmt='%Y-%m-%d %H:%M:%S')
+    logging.info(msg)
