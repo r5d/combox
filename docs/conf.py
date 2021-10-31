971761d82115d023c4d1a9870deab6a269d18495
siddharth <s@ricketyspace.net>
2021-10-31 16:26:35 -0400

    Revert "docs/conf.py: remove intersphinx_mapping"
    This reverts commit 57f2504cfc7d89c519b35952400e3b09309674b7.

--- docs/conf.py
+++ docs/conf.py
@@ -298,3 +298,9 @@ texinfo_documents = [
 # If true, do not generate a @detailmenu in the "Top" node's menu.
 #texinfo_no_detailmenu = False
 
+
+# Example configuration for intersphinx: refer to the Python standard library.
+intersphinx_mapping = {
+    'https://docs.python.org/2.7': None,
+    'https://pythonhosted.org/watchdog/': None
+}
