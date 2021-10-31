476ed4a035616a2a702cc26c9438209561eb6fd3
siddharth <s@ricketyspace.net>
2021-10-31 15:29:53 -0400

    setup.py: install_requires: update PyYAML
    Install PyYAML 5.4.1.1; last release compatible with python 2.7

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ config = {
     'download_url': 'https://git.ricketyspace.net/combox',
     'author': 'combox contributors',
     'author_email': 'sravik@bgsu.edu',
-    'install_requires': ['watchdog', 'PyYAML', 'pycrypto',
+    'install_requires': ['watchdog', 'PyYAML==5.4.1.1', 'pycrypto',
                          'simplejson', 'pickledb'],
     'tests_require': ['nose', 'mock'],
     'test_suite': 'nose.collector',
