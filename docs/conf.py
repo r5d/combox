063fdef272f665daf756d529ef1d12cc757f99a7
siddharth <s@ricketyspace.net>
2021-10-31 15:47:01 -0400

    docs/conf.py: update intersphinx_mapping

--- docs/conf.py
+++ docs/conf.py
@@ -301,6 +301,6 @@ texinfo_documents = [
 
 # Example configuration for intersphinx: refer to the Python standard library.
 intersphinx_mapping = {
-    'https://docs.python.org/': None,
+    'https://docs.python.org/2.7': None,
     'https://pythonhosted.org/watchdog/': None
 }
